f7f8271e1ed87280ca7319dd040e8e7a4b2b6f64
tanourine <taltrmous@gmail.com>
2025-06-23 00:20:30 +0400

    D

--- lib/drawer_menu.dart
+++ lib/drawer_menu.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-
-// استورد هنا الصفحات التي أنشأتها في مجلد lib
-import 'home_page.dart';
 import 'income_invoice_page.dart';
 import 'request_money_page.dart';
 import 'tools_check_page.dart';
@@ -11,39 +8,8 @@ import 'warning_banner.dart';
 import 'upload_tools_images_page.dart';
 import 'location_tracking_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'تنورين للتكييف',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) => const HomePage(),
-        '/income': (context) => const IncomeInvoicePage(),
-        '/request-money': (context) => const RequestMoneyPage(),
-        '/tools-check': (context) => const ToolsCheckPage(),
-        '/money-review': (context) => const RequestReviewPage(),
-        '/excel-export': (context) => const ExcelExportPage(),
-        '/income-warning': (context) => const WarningBannerPage(),
-        '/admin-tools': (context) => const UploadToolsImagesPage(),
-        '/location-tracking': (context) => const LocationTrackingPage(),
-      },
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class DrawerMenu extends StatelessWidget {
+  const DrawerMenu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +30,6 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            _drawerItem(context, 'الصفحة الرئيسية', '/home'),
             _drawerItem(context, 'فاتورة دخل', '/income'),
             _drawerItem(context, 'طلب أموال', '/request-money'),
             _drawerItem(context, 'تشييك العدة', '/tools-check'),
--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,42 @@
 import 'package:flutter/material.dart';
 
-// استيراد قائمة التنقل (الـ Drawer) والصفحات المختلفة
+// استورد DrawerMenu بدلاً من HomePage المكرر
 import 'drawer_menu.dart';
-import 'home_page.dart';
-import 'login_page.dart';
-import 'expense_invoice_page.dart';
 import 'income_invoice_page.dart';
 import 'request_money_page.dart';
+import 'tools_check_page.dart';
 import 'request_review_page.dart';
 import 'excel_export_page.dart';
-import 'location_tracking_page.dart';
-import 'settings_page.dart';
-import 'tools_check_page.dart';
-import 'upload_tools_images_page.dart';
 import 'warning_banner.dart';
+import 'upload_tools_images_page.dart';
+import 'location_tracking_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'تطبيقي متعدد الصفحات',
-      debugShowCheckedModeBanner: false,
+      title: 'تنورين للتكييف',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        scaffoldBackgroundColor: Colors.grey[50],
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      // يبدأ التطبيق بقائمة التنقل التي تتولى عرض الـ Scaffold والـ Drawer
+      // نجعل DrawerMenu هو الشاشة الرئيسية
       home: const DrawerMenu(),
-
-      // تعريف المسارات لتمكين التنقل المسمّى
       routes: {
-        '/home': (_) => const HomePage(),
-        '/login': (_) => const LoginPage(),
-        '/expense': (_) => const ExpenseInvoicePage(),
         '/income': (_) => const IncomeInvoicePage(),
-        '/request_money': (_) => const RequestMoneyPage(),
-        '/request_review': (_) => const RequestReviewPage(),
-        '/export_excel': (_) => const ExcelExportPage(),
-        '/location': (_) => const LocationTrackingPage(),
-        '/settings': (_) => const SettingsPage(),
-        '/tools_check': (_) => const ToolsCheckPage(),
-        '/upload_tools_images': (_) => const UploadToolsImagesPage(),
-        '/warning': (_) => const WarningBanner(),
+        '/request-money': (_) => const RequestMoneyPage(),
+        '/tools-check': (_) => ToolsCheckPage(),            // إذا أردت تجعلها const أضف const في الـ constructor
+        '/money-review': (_) => RequestReviewPage(),
+        '/excel-export': (_) => const ExcelExportPage(),
+        '/income-warning': (_) => const WarningBannerPage(),
+        '/admin-tools': (_) => AdminToolsUploadPage(),
+        '/location-tracking': (_) => const LocationTrackingPage(),
       },
     );
   }
