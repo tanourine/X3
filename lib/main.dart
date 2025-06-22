1223888eb5e8bf9218db9c30a00f6479f3829ac0
tanourine <taltrmous@gmail.com>
2025-06-23 00:26:29 +0400

    S

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
-// استورد DrawerMenu بدلاً من HomePage المكرر
-import 'drawer_menu.dart';
+// استورد هنا جميع الصفحات الموجودة في مجلّد lib
+import 'home_page.dart';
 import 'income_invoice_page.dart';
 import 'request_money_page.dart';
 import 'tools_check_page.dart';
 import 'request_review_page.dart';
 import 'excel_export_page.dart';
-import 'warning_banner.dart';
-import 'upload_tools_images_page.dart';
+import 'warning_banner.dart';            // إذا كان اسم الصف المعرّف فيه هو WarningBanner
+import 'upload_tools_images_page.dart'; // يعرّف AdminToolsUploadPage
 import 'location_tracking_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,17 +24,17 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      // نجعل DrawerMenu هو الشاشة الرئيسية
-      home: const DrawerMenu(),
+      initialRoute: '/home',
       routes: {
-        '/income': (_) => const IncomeInvoicePage(),
-        '/request-money': (_) => const RequestMoneyPage(),
-        '/tools-check': (_) => ToolsCheckPage(),            // إذا أردت تجعلها const أضف const في الـ constructor
-        '/money-review': (_) => RequestReviewPage(),
-        '/excel-export': (_) => const ExcelExportPage(),
-        '/income-warning': (_) => const WarningBannerPage(),
-        '/admin-tools': (_) => AdminToolsUploadPage(),
-        '/location-tracking': (_) => const LocationTrackingPage(),
+        '/home':                (context) => HomePage(),
+        '/income':              (context) => IncomeInvoicePage(),
+        '/request-money':       (context) => RequestMoneyPage(),
+        '/tools-check':         (context) => ToolsCheckPage(),
+        '/money-review':        (context) => RequestReviewPage(),
+        '/excel-export':        (context) => ExcelExportPage(),
+        '/income-warning':      (context) => WarningBanner(),            // أو WarningBannerPage() حسب الملف
+        '/admin-tools':         (context) => AdminToolsUploadPage(),
+        '/location-tracking':   (context) => LocationTrackingPage(),
       },
     );
   }
